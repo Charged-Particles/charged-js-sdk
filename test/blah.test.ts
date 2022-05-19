07ce2d368b20b7fb5aff7c96bba1c37fc96c917e
scammi <scammi@gmail.com>
2022-05-19 18:11:33 -0300

    Get charged particle contract for class

--- test/blah.test.ts
+++ test/blah.test.ts
@@ -17,11 +17,19 @@ describe('getStateAddressTest', () => {
 })
 
 describe('Charged class', () => {
-  it('Should create the class', () => {
-    const provider = ethers.providers.getDefaultProvider();
-    const charged = new Charged('RPC_URL',provider);
+  const provider = ethers.providers.getDefaultProvider();
+  const charged = new Charged('RPC_URL',provider);
 
+  it('Should create the class', async () => {
     expect(charged.RPC_URL).toEqual('RPC_URL');
+    expect(await charged.provider.getNetwork()).toHaveProperty('chainId');
+  })
 
+  it('Get charged particle contract', async () => {
+    // const charged = new Charged('RPC_URL',provider);
+    const chargeParticleContract = charged.getChargeParticleContract();
+    const stateContractAddress = await chargeParticleContract.getStateAddress();
+
+    expect(stateContractAddress).toEqual('0x48974C6ae5A0A25565b0096cE3c81395f604140f');
   })
 })
