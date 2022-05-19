b24464c56433b54f1461abd6589f0affb4e9de1c
scammi <scammi@gmail.com>
2022-05-19 16:24:50 -0300

    Import json working

---
+++ src/ChargedParticles.ts
@@ -0,0 +1,7 @@
+// import { signText } from "./ethers.service";
+import ChargedParticles from "./abis/v2/ChargedParticles.json";
+
+export const getStateAddress = () => {
+   // create trx for state address
+   console.log(ChargedParticles)
+}
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,13 @@
 // import { RPC_URL, MNEMONIC } from './config';
-import { getAddressFromSigner } from "./ethers.service";
+// import { getAddressFromSigner } from "./ethers.service";
+import { getStateAddress } from "./ChargedParticles";
 
 export const sum = (a: number, b: number) => {
   if ('development' === process.env.NODE_ENV) {
     console.log('boop');
   }
 
-  console.log(getAddressFromSigner());
+  console.log(getStateAddress());
   return a + b;
 };
 
