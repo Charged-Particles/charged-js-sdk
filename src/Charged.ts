985b346bca174577c961981a604995bcd2be85c1
scammi <scammi@gmail.com>
2022-05-19 17:49:39 -0300

    Class and basic test

---
+++ src/Charged.ts
@@ -0,0 +1,34 @@
+import { ethers, providers } from "ethers";
+import ChargedParticles from "./abis/v2/ChargedParticles.json";
+
+export default class Charged  {
+  RPC_URL: String;
+  provider: providers.Provider;
+
+  constructor(_RPC_URL: String, _provider: providers.Provider) {
+    this.RPC_URL = _RPC_URL;
+    this.provider = _provider;
+  }
+
+  getChargeParticleContract() {
+    const contract = new ethers.Contract(
+      '0xaB1a1410EA40930755C1330Cc0fB3367897C8c41',
+      ChargedParticles,
+      this.provider
+    );    
+
+   return contract
+  }
+
+}
+
+
+/*
+  const charge = new Charge(...)
+
+  charge.getStateAddress
+
+  const chargedParticleContract = charged.getChargeParticleContract()
+  chargedParticleContract.getStateAddress;
+
+*/
--- test/blah.test.ts
+++ test/blah.test.ts
@@ -1,14 +1,27 @@
-// import { sum } from '../src';
 import { getStateAddress } from '../src/ChargedParticles';
+import { sum } from '../src';
+import Charged from '../src/Charged';
+import { ethers } from 'ethers'
 
-// describe('blah', () => {
-//   it('works', () => {
-//     expect(sum(1, 1)).toEqual(2);
-//   });
-// });
+
+describe('blah', () => {
+  it('works', () => {
+    expect(sum(1, 1)).toEqual(2);
+  });
+});
 
 describe('getStateAddressTest', () => {
   it('returns a string', async () => {
     expect(await getStateAddress()).toEqual('0x48974C6ae5A0A25565b0096cE3c81395f604140f');
+  })
+})
+
+describe('Charged class', () => {
+  it('Should create the class', () => {
+    const provider = ethers.providers.getDefaultProvider();
+    const charged = new Charged('RPC_URL',provider);
+
+    expect(charged.RPC_URL).toEqual('RPC_URL');
+
   })
 })
