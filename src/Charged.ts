1c6b84182c2b058afad702e1988cd7dbe1cb1492
scammi <scammi@gmail.com>
2022-05-20 14:50:35 -0300

    Init charged from PK if no provider was passed

--- src/Charged.ts
+++ src/Charged.ts
@@ -1,18 +1,40 @@
 import { ethers, providers } from "ethers";
+import { Networkish } from "@ethersproject/networks";
 import ChargedParticles from "./abis/v2/ChargedParticles.json";
 import { getStateAddress } from "./ChargedParticles";
 
 export default class Charged  {
   RPC_URL: String;
   provider: providers.Provider;
+  network: Networkish;
 
-  constructor(_RPC_URL: String, _provider: providers.Provider) {
+  constructor(
+   _RPC_URL: String,
+   _provider: providers.Provider,
+   _network: Networkish
+   ) {
+
     this.RPC_URL = _RPC_URL;
     this.provider = _provider;
+    this.network = _network;
+
+    if (!_provider) {
+      if (Boolean(process.env.PK)) {
+        this.provider = ethers.getDefaultProvider(_network, process.env.PK);
+      } else {
+        this.provider = ethers.getDefaultProvider(_network);
+        console.log(
+          `These API keys are a provided as a community resource by the backend services for low-traffic projects and for early prototyping.
+          It is highly recommended to use own keys: https://docs.ethers.io/v5/api-keys/`
+        );
+      }
+    } else {
+      this.provider = _provider;
+    }
   }
 
   getStateAddress = getStateAddress;
-
+  
   getChargeParticleContract() {
     const contract = new ethers.Contract(
       '0xaB1a1410EA40930755C1330Cc0fB3367897C8c41',
--- src/ChargedParticles.ts
+++ src/ChargedParticles.ts
@@ -1,11 +1,11 @@
 import ChargedParticles from "./abis/v2/ChargedParticles.json";
 import mainnetAddresses from './networks/v2/mainnet.json';
-import { ethers } from 'ethers';
+import { ethers, providers } from 'ethers';
 
 // @dev returns the state adress from the ChargedParticles contract
 // @param provider - optional parameter. if not defined the code will use the ethers default provider.
 // @returns string of state address
-export const getStateAddress = async (provider?:ethers.providers.JsonRpcProvider) => {
+export const getStateAddress = async (provider?: providers.Provider) => {
    const defaultProvider:ethers.providers.BaseProvider = ethers.providers.getDefaultProvider();
    const contract:ethers.Contract = new ethers.Contract(
       mainnetAddresses.chargedParticles.address,
