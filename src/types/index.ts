2370c03d772fb347a146533fe620c5ceb89bbc62
Kamran Choudhry <theblizzzzard@gmail.com>
2022-05-30 14:32:31 -0400

    EDIT: clean up types file

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,6 +1,8 @@
 import { ethers, providers, Signer } from 'ethers';
-import { Networkish } from "@ethersproject/networks";
 
+export type MultiProvider = providers.JsonRpcProvider | providers.Provider;
+
+export type MultiSigner = ethers.Signer |ethers.VoidSigner | ethers.Wallet | providers.JsonRpcSigner;
 
 export type DefaultProviderKeys = {
   etherscan?: string;
@@ -13,23 +15,7 @@ export type networkProvider = {
   service: any
 }
 
-export type MultiProvider = providers.JsonRpcProvider | providers.Provider;
-
-export type MultiSigner = ethers.Signer |ethers.VoidSigner | ethers.Wallet | providers.JsonRpcSigner;
-
 export type Configuration = {
   providers?: any;
   signer?: Signer;
 };
-
-export type constructorParams = {
-  providers?: networkProvider[],  
-  signer?: Signer,
-};
-
-export interface charged {
-  network?: Networkish,
-  provider?: providers.Provider | providers.ExternalProvider | string,
-  signer?: Signer,
-  defaultProviderKeys?: DefaultProviderKeys
-}
