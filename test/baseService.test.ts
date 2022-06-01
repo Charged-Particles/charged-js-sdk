70e3e0974933fe34599bf89c970638f57969b01c
Kamran Choudhry <theblizzzzard@gmail.com>
2022-06-01 16:16:04 -0400

    NEW: test for baseService

---
+++ test/baseService.test.ts
@@ -0,0 +1,37 @@
+import Charged from '../src/Charged';
+
+describe('baseService class', () => {
+    const providers =  [
+      {
+        network: 1,
+        service: {'alchemy': 'qw02QqWNMg2kby3q3N39PxUT3KaRS5UE'}
+      },
+      {
+        network: 42,
+        service: {'alchemy': 'rm-l6Zef1007gyxMQIwPI8rEhaHM8N6a'}
+      }
+    ]
+  
+    it ('get tokens across more than one network', async () => {
+      const charged = new Charged({providers})
+      const particleBAddress = '0x517fEfB53b58Ec8764ca885731Db20Ca2dcac7b7';
+      const tokenId = 4;
+    
+
+      const data = await charged.utils.storeTokenIdsAccrossChains(particleBAddress, tokenId);
+  
+      // check the that keys exist for one network only
+      expect(data[0]).toHaveProperty('tokenId');
+      expect(data[0]).toHaveProperty('chainId');
+      expect(data[0]).toHaveProperty('ownerOf');
+
+      // check that the values are correct aswell
+      expect(data[0].tokenId).toEqual(4);
+      expect(data[0].chainId).toEqual(42);
+      expect(data[0].ownerOf).toEqual('0x6d46b37708dA7Ed4E5C4509495768Fecd3D17C01');
+  
+  
+      console.log({data});
+    });
+  
+});
