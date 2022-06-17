6966249c397e113a16ee9d91356c06400821e207
scammi <scammi@gmail.com>
2022-06-17 11:06:42 -0300

    Check params for chargeState test

--- test/chargedState.test.ts
+++ test/chargedState.test.ts
@@ -11,7 +11,7 @@ const providersKovan = [
     network: 42,
     service: { 'alchemy': process.env.ALCHEMY_KOVAN_KEY }
   }
-]
+];
 const expectedUnlockBlockNumber = 32174859;
 const walletAddress = '0x277bfc4a8dc79a9f194ad4a83468484046fafd3a';
 const tokenAddress = '0xd1bce91a13089b1f3178487ab8d0d2ae191c1963';
@@ -30,30 +30,86 @@ describe('ChargedState contract test', () => {
   it('Set release timelock', async () => {
     const result = await nft.releaseTimelock(expectedUnlockBlockNumber);
     expect(result).toEqual(true);
+
+    expect(writeContractMock.mock.calls[0][0]).toBe('chargedState');
+    expect(writeContractMock.mock.calls[0][1]).toBe('setReleaseTimelock');
+    expect(writeContractMock.mock.calls[0][2]).toBe(42);
+    expect(writeContractMock.mock.calls[0][3]).toEqual([
+      tokenAddress,
+      tokenId,
+      expectedUnlockBlockNumber 
+    ]);
   });
 
   it('Get release timelock', async () => {
     const releaseState = await nft.getReleaseState(walletAddress);
     expect(releaseState['42'].value).toEqual('success');
+
+    expect(readContractMock.mock.calls[0][0]).toBe('chargedState');
+    expect(readContractMock.mock.calls[0][1]).toBe('getReleaseState');
+    expect(readContractMock.mock.calls[0][2]).toBe(42);
+    expect(readContractMock.mock.calls[0][3]).toEqual([
+      tokenAddress,
+      tokenId,
+      walletAddress
+    ]);
   });
 
   it('Set discharge timelock', async () => {
     const result = await nft.dischargeTimelock(expectedUnlockBlockNumber);
     expect(result).toEqual(true);
+
+    expect(writeContractMock.mock.calls[0][0]).toBe('chargedState');
+    expect(writeContractMock.mock.calls[0][1]).toBe('setDischargeTimelock');
+    expect(writeContractMock.mock.calls[0][2]).toBe(42);
+    expect(writeContractMock.mock.calls[0][3]).toEqual([
+      tokenAddress,
+      tokenId,
+      expectedUnlockBlockNumber 
+    ]); 
   });
 
   it('Set get discharge timelock', async () => {
     const dischargeState = await nft.getDischargeState(walletAddress);
     expect(dischargeState['42'].value).toEqual('success');
+
+    expect(readContractMock.mock.calls[0][0]).toBe('chargedState');
+    expect(readContractMock.mock.calls[0][1]).toBe('getDischargeState');
+    expect(readContractMock.mock.calls[0][2]).toBe(42);
+    expect(readContractMock.mock.calls[0][3]).toEqual([
+      tokenAddress,
+      tokenId,
+      walletAddress
+    ]);
   });
 
   it('Set bonds timelock', async () => {
     const result = await nft.bondsTimelock(expectedUnlockBlockNumber);
     expect(result).toEqual(true);
+
+    expect(writeContractMock.mock.calls[0][0]).toBe('chargedState');
+    expect(writeContractMock.mock.calls[0][1]).toBe('setBreakBondTimelock');
+    expect(writeContractMock.mock.calls[0][2]).toBe(42);
+    expect(writeContractMock.mock.calls[0][3]).toEqual([
+      tokenAddress,
+      tokenId,
+      expectedUnlockBlockNumber
+    ]);
   });
 
   it('Get bonds timelock', async () => {
     const bondsState = await nft.getBondsState(walletAddress);
+
     expect(bondsState['42'].value).toEqual('success');
+
+    expect(readContractMock.mock.calls[0][0]).toBe('chargedState');
+    expect(readContractMock.mock.calls[0][1]).toBe('getBreakBondState');
+    expect(readContractMock.mock.calls[0][2]).toBe(42);
+    expect(readContractMock.mock.calls[0][3]).toEqual([
+      tokenAddress,
+      tokenId,
+      walletAddress
+    ]);
   });
+
 });
