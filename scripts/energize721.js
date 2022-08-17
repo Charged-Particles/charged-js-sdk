4fe66b49336f867bb7b869a65306413177e360c1
scammi <scammi@gmail.com>
2022-08-17 14:48:48 -0300

    Working scripts fetches amount

--- scripts/energize721.js
+++ scripts/energize721.js
@@ -1,20 +1,24 @@
 const hre = require('hardhat');
-const { default: Charged, kovanAddresses, protonBAbi } = require('../dist/index')
+const { default: Charged, goerliAddresses, protonBAbi } = require('../dist/index')
 
 async function main() {
   const signer = await hre.ethers.getSigner();
 
   // Give permit to the charged contract.
-  const depositedNft = new hre.ethers.Contract('0x1299B7236b43557F077f4BEBc417Ae96E0A82671', protonBAbi, signer);
-  const approveTx = depositedNft.approve(kovanAddresses.chargedParticles.address, '1');
-  await approveTx.wait();
+  // const depositedNft = new hre.ethers.Contract('0x07bc6CeEe891E66395277618d2859AEdeB21d262', protonBAbi, signer);
+  // const depositedNftConnected = depositedNft.connect(hre.ethers.provider);
+  // const approveTx = await depositedNft.approve(goerliAddresses.chargedParticles.address, 1);
+  // await approveTx.wait();
   
   const charged = new Charged({providers: hre.ethers.provider, signer});
-  const nft = charged.NFT('0xd1bce91a13089b1f3178487ab8d0d2ae191c1963', 57);
-  const bondTx = await nft.bond('0x1299B7236b43557F077f4BEBc417Ae96E0A82671', '1', '1');
-  const bondReceipt = await bondTx.wait();
+  const nft = charged.NFT('0x894fe586f4be12cdb5d107323a2f5182161c3515', 6);
+  // const bondTx = await nft.bond('0x07bc6CeEe891E66395277618d2859AEdeB21d262', '1', '1', 'generic.B');
+  // const bondReceipt = await bondTx.wait();
+  // console.log(bondReceipt);
 
-  console.log(bondReceipt);
+  console.log(await nft.getBonds());
+
+
 }
 
 // We recommend this pattern to be able to use async/await everywhere
