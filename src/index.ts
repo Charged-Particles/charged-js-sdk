4a7b243dc454418084af38d6e1b84d6af850a248
scammi <scammi@gmail.com>
2023-01-12 16:06:21 -0300

    update dependency to allow listed repo

--- src/index.ts
+++ src/index.ts
@@ -16,7 +16,6 @@ import genericBasketManagerBAbi from '@charged-particles/protocol-subgraph/abis/
 
 // contract-allowlist
 import { allowlist } from '@charged-particles/contract-allowlist/src/allowlist';
-// import allowlist
 
 // networks
 import mainnetAddresses from '@charged-particles/protocol-subgraph/networks/mainnet.json';
